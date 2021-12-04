bc15ff5f8cf26226ed53250259dc9a092b7af1ff
h5jam <so.h4ns@gmail.com>
2021-12-04 21:54:53 +0900

    Create search buttion

--- reclothes/SearchViewController.swift
+++ reclothes/SearchViewController.swift
@@ -15,12 +15,25 @@ class SearchViewController: UIViewController {
     }
     
     func setupSearchController() {
+        // style 1
         let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.placeholder = "검색해 줘"
         searchController.hidesNavigationBarDuringPresentation = false
         
         self.navigationItem.searchController = searchController
         self.navigationItem.title = "Search"
         self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        // style 2
+        //        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: view.frame.width - 80, height: 0))
+        //        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: searchBar)
+        //
+        //        self.navigationItem.title = "Search"
+        //        self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        let search = UIBarButtonItem(systemItem: .search, primaryAction: UIAction(handler: { _ in
+            // To do: 검색 결과 페이지로 연결
+        }))
+        self.navigationItem.rightBarButtonItem = search
     }
 }
