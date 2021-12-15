b4e6e2b8dc5efc2d63e84d48710e6af47d165b17
h5jam <so.h4ns@gmail.com>
2021-12-16 00:39:39 +0900

    찜목록 테이블에 데이터 추가

--- reclothes/ViewController/LikesViewController.swift
+++ reclothes/ViewController/LikesViewController.swift
@@ -7,10 +7,37 @@
 
 import UIKit
 
-class LikesViewController: UIViewController {
+class LikesViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    let cellIdentifier: String = "cell"
+    
+    // 데이터를 저장할 곳
+    let contents: [String] = ["파자마", "원피스"]
+    let memo: [String] = ["5000원", "7000원"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = self.contents[indexPath.row]
+        return cell
+    }
+    
+    // 셀 크기
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+	
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.register(UITableViewCell.self,forCellReuseIdentifier: cellIdentifier)
+        
         navigationController?.navigationBar.topItem?.title = "찜"
         // Do any additional setup after loading the view.
     }
