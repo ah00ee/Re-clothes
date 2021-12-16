f64fb6ee695baf7e8c4441ba2eedb8facce0670a
dayo2n <0217dayun@naver.com>
2021-12-17 01:25:02 +0900

    add sample images in category view

--- reclothes/ViewController/ItemCollectionViewController.swift
+++ reclothes/ViewController/ItemCollectionViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 // 검색 결과로 보이는 아이템 콜렉션 뷰
 class ItemCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    var tmpItemName = ["구두", "아우터", "가방", "후드", "악세사리", "구두", "아우터", "가방", "후드", "악세사리", "구두", "아우터", "가방", "후드", "악세사리"]
+    var imgName = ["dress01", "dress02", "dress03", "dress04", "dress05", "dress06", "dress07", "dress08", "dress09", "dress10", "dress11", "dress12", "dress13", "dress14"]
+    var tmpItemName = ["자켓 투피스", "수트", "여성 정장", "체크패턴 수트", "버튼 원피스", "트위드 투피스", "원피스", "레이스 핀턱 미니원피스", "리사 기모 후드", "스커트", "롱스커트", "미디움 스커트", "플로럴 스커트", "포인트 스커트"]
     var tmpItemPrice = ["2000원", "4400원", "2300원", "3000원", "30000원", "2000원", "4400원", "2300원", "3000원", "30000원", "2000원", "4400원", "2300원", "3000원", "30000원"]
     let sectionInsets = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
 
@@ -24,7 +25,7 @@ class ItemCollectionViewController: UIViewController, UICollectionViewDelegate, 
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return 14
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -34,7 +35,7 @@ class ItemCollectionViewController: UIViewController, UICollectionViewDelegate, 
         cell.layer.borderColor = UIColor.gray.cgColor
         
         // 여기 가격 안보임, 셀 사이즈 조정 : 라인 간격도 
-        cell.itemImage.image = nil
+        cell.itemImage.image = UIImage(named: imgName[indexPath.row])
         cell.itemTitle.text = tmpItemName[indexPath.row]
         cell.itemPrice.text = tmpItemPrice[indexPath.row]
         cell.itemPrice.text! += "(1일기준)"
